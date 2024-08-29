dfb51382f0805c60a291b8581c44b18f35035b43
brunofhorn <b.fernandeshorn@gmail.com>
2024-08-28 23:43:54 -0300

    feat: add lamborghini fake project

---
+++ lamborghini-fake/README.md
@@ -0,0 +1,4 @@
+Desafio da dio.me .
+Como Consumir API em APPs React Native na Prática.
+Aplicativo FAKE Lamborghini.
+Seleção de carros usando várias técnicas importantes.
---
+++ lamborghini-fake/src/constants/car.ts
@@ -0,0 +1,4 @@
+export const CAR_ASSETS_BASE_URL = "https://raw.githubusercontent.com/digitalinnovationone/fake-data-api-lamborghini/main/assets/";
+
+
+export const CAR_API_BASE_URL ="https://digitalinnovationone.github.io/fake-data-api-lamborghini/api/lamborghini.json";
